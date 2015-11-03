5856478bda75ac7ae94aace893a377c5238d4ccb
oviava <ovidiu.avasilcai@scc.com>
2015-11-03 11:24:50 +0200

    inital commit slides

---
+++ src/components/wrappers/Slide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Slide = ({children}) => {
+  return (
+    <div className="container">
+      {children}
+    </div>
+  );
+};
+
+export default Slide;
---
+++ src/reducers/initialData.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const initialData = {
+  1: {
+    (<Slide />),
+  },
+  2 : {
+
+  },
+  3 : {
+
+  },
+  4 : {
+
+  },
+};
+
+export default initialData;
